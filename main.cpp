529fe1af367577d195e368de52601db25a1afcf5
Jollu8 <zholu8@gmail.com>
2023-10-23 16:03:19 +0300

    added work 2,3,4

--- main.cpp
+++ main.cpp
@@ -1,15 +1,48 @@
 #include <cstdlib>
 #include <fstream>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <string>
 
-#include <cstdlib>
-#include <fstream>
+const std::string run_ipFilter = "./temp_exec/work_2_FilterIP/ipFilter";
+const char *build_ipFilter = "mkdir -p ./temp_exec/work_2_FilterIP && cd ./temp_exec/work_2_FilterIP && cmake ../../src/work_2_FilterIP/ && make";
+
+const std::string run_work_3 = "./temp_exec/work_3_allocator/allocator";
+const char * build_work_3 = "mkdir -p ./temp_exec/work_3_allocator && cd ./temp_exec/work_3_allocator && cmake ../../src/work_3_allocator/ && make";
+
+
+const std::string run_work_4 = "./temp_exec/work_4_SFINAE/print_ip";
+const char * build_work_4 = "mkdir -p ./temp_exec/work_4_SFINAE && cd ./temp_exec/work_4_SFINAE && cmake ../../src/work_4_SFINAE/ && make";
 
 int main() {
-    std::ifstream ifile("./temp_exec/work_2_FilterIP/ipfilter");
-    if(!ifile) {
-        std::system("mkdir -p ./temp_exec/work_2_FilterIP && cd ./temp_exec/work_2_FilterIP && cmake ../../src/work_2_FilterIP/ && make");
-    }
-    system("./temp_exec/work_2_FilterIP/ipfilter ./data/ip_filter.tsv");
+    using namespace std::chrono_literals;
+
+
+
+
+//    std::cout << "\n\n=============== work_2_FilterIP ====================\n" << std::endl;
+//    std::ifstream ipFilter(run_ipFilter);
+//    if (!ipFilter)
+//        std::system(build_ipFilter);
+//    std::system(std::string(run_ipFilter + " ./data/ip_filter.tsv").c_str());
+//    std::this_thread::sleep_for(2000ms);
+//
+//
+//    std::cout << "\n\n================= work_3_allocator =================\n" << std::endl;
+//    std::ifstream work_3(run_work_3);
+//    if (!work_3)
+//        std::system(build_work_3);
+//    std::system(run_work_3.c_str());
+//    std::this_thread::sleep_for(2000ms);
+
+    std::cout << "\n\n================= work_4_SFINAE =================\n" << std::endl;
+    std::ifstream work_4(run_work_4);
+    if (!work_4)
+        std::system(build_work_4);
+    std::system(run_work_4.c_str());
+    std::this_thread::sleep_for(2000ms);
+
 }
 
 
